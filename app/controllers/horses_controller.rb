30cda5812b2bba90e89b544b3a9373c828defa22
Wilson Reid <wilsonthehungryman@gmail.com>
2025-02-04 13:07:54 -0600

    Implemented the HorsesController

--- app/controllers/horses_controller.rb
+++ app/controllers/horses_controller.rb
@@ -1,7 +1,10 @@
 class HorsesController < ApplicationController
   def show
+    @horse = Horse.find(params[:id])
   end
 
   def search_results
+    @query = params[:query]
+    @horses = Horse.where("name LIKE ?", "%#{@query}%")
   end
 end
