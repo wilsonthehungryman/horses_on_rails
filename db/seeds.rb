bc864a5bedcfd3fbd55afeea00d6b63afb2fb84b
Wilson Reid <wilsonthehungryman@gmail.com>
2025-02-04 13:00:06 -0600

    Added the seed script along with gems for faker and rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Horse.destroy_all
+Breed.destroy_all
+
+NUMBER_OF_BREEDS = 5
+HORSES_PER_BREED = 15
+
+NUMBER_OF_BREEDS.times do
+  breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
+
+  HORSES_PER_BREED.times do
+    breed.horses.create(
+      name: Faker::Creature::Horse.unique.name,
+      age: rand(1..587).to_i,
+      number_of_legs: rand(2..222).to_i,
+      top_speed: 15 + rand * 100
+    )
+  end
+end
+
+puts "Created #{Breed.count} Breeds."
+puts "Created #{Horse.count} Horses."
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
