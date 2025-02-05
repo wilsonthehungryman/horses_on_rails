d298d7dd2d41c7b9a0fb3c552103c8d6ef518802
Wilson Reid <wilsonthehungryman@gmail.com>
2025-02-05 16:41:46 -0600

    Added validations to Horse and Breed models

--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,3 +1,5 @@
 class Breed < ApplicationRecord
   has_many :horses
+
+  validates :name, presence: true
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,3 +1,6 @@
 class Horse < ApplicationRecord
   belongs_to :breed
+
+  validates :name, :age, :number_of_legs, :top_speed, presence: true
+  validates :age, :number_of_legs, numericality: { only_integer: true }
 end
