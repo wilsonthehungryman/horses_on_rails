e6b516861e2203aa4e5ffa73a2f6750457bea1d4
Wilson Reid <wilsonthehungryman@gmail.com>
2025-02-06 10:59:45 -0600

    Implemented Pexels in the seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 Horse.destroy_all
 Breed.destroy_all
 AdminUser.destroy_all
@@ -5,16 +7,22 @@ AdminUser.destroy_all
 NUMBER_OF_BREEDS = 5
 HORSES_PER_BREED = 15
 
+image_client = Pexels::Client.new()
+response = image_client.photos.search('horse', page: 1, per_page: 100)
+
 NUMBER_OF_BREEDS.times do
   breed = Breed.create(name: Faker::Creature::Horse.unique.breed)
 
-  HORSES_PER_BREED.times do
-    breed.horses.create(
+  HORSES_PER_BREED.times do |index|
+    horse = breed.horses.create(
       name: Faker::Creature::Horse.unique.name,
       age: rand(1..587).to_i,
       number_of_legs: rand(2..222).to_i,
       top_speed: 15 + rand * 100
     )
+
+    downloaded_image = URI.parse(response.photos[index].src["small"]).open
+    horse.image.attach(io: downloaded_image, filename: "#{horse.name} - #{breed.name}.jpg")
   end
 end
 
